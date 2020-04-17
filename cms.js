71f5ccf569f98c07762b43755221b72c9b8b38a4
Maxime Moreillon <m.moreillon@gmail.com>
2020-04-17 02:11:40 +0000

    debugging

--- cms.js
+++ cms.js
@@ -38,9 +38,9 @@ function return_user_id(res) {
 
 app.get('/', (req, res) => {
   res.send(`
-    CMS API, Maxime MOREILLON \n
-    Authentication API URL: ${secrets.authentication_api_url} \n
-    Neo4J URL: ${secrets.neo4j.url} \n
+    CMS API, Maxime MOREILLON <br>
+    Authentication API URL: ${secrets.authentication_api_url} <br>
+    Neo4J URL: ${secrets.neo4j.url} <br>
     `)
 })
 

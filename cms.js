0097600ddac1bba0c793e52fcc7a41bc369b5f72
Maxime Moreillon <m.moreillon@gmail.com>
2020-09-26 15:43:01 +0000

    root route

--- cms.js
+++ cms.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const dotenv = require('dotenv')
+const pjson = require('./package.json')
 
 dotenv.config()
 
@@ -14,7 +15,15 @@ const app = express()
 app.use(bodyParser.json({limit: '50mb', extended: true}))
 app.use(cors())
 
-app.get('/', (req, res) => { res.send(`CMS API, Maxime MOREILLON`) })
+app.get('/', (req, res) => {
+  res.send({
+    application_name: 'CMS',
+    author: 'Maxime MOREILLON',
+    version: pjson.version,
+    neo4j_url: process.env.NEO4J_URL,
+    authentication_api_url: process.env.AUTHENTICATION_API_URL,
+  })
+})
 
 app.use('/articles', require('./routes/articles.js'))
 app.use('/tags', require('./routes/tags.js'))

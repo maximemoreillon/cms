1e050f3f500432082ecd99d721aae4722692728a
Maxime Moreillon <m.moreillon@gmail.com>
2021-12-19 02:10:28 +0000

    auth

--- identification.js
+++ identification.js
@@ -1,6 +1,9 @@
 module.exports =  (res) => {
 
   const current_user = res.locals.user
+
+  if(!current_user) return undefined
+  
   return current_user._id
     ?? current_user.properties._id
     ?? current_user.identity.low
